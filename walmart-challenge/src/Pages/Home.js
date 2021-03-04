9ee5904f9ad27139dccd320870a2dbf4d815b082
Amitesh Sharma <amitesh.sharma.2001@gmail.com>
2021-03-03 19:33:43 -0800

    home page frontend is complete, next work on the fetch call and then the table

--- walmart-challenge/src/Pages/Home.js
+++ walmart-challenge/src/Pages/Home.js
@@ -1,20 +1,28 @@
 import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
-import { withStyles } from '@material-ui/styles';
-import TextField from '@material-ui/core/TextField'; 
+import { Container, Row, Col, Form, Button } from 'react-bootstrap';
+// import { withStyles } from '@material-ui/styles';
+// import TextField from '@material-ui/core/TextField'; 
 import '../Css/Home.css';
 
-const styles = theme => ({
-    textfield: {
-        width: 'calc(40vw)',
-        color: 'white',
-        backgroundColor: 'white'
-    },
-  });
-
 class Home extends React.Component {
-    render() {
-        const classes = this.props; 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            GithubURL: '',
+            Error: false,
+        }
+
+        this.getData = this.getData.bind(this);
+    }
+
+    getData(url) {
+        let fetchUrl = url ? url : 'some value';
+
+    }
+
+
+    render() { 
         return (
             <div className="home-page">
                 <section className="section-description">
@@ -24,20 +32,31 @@ class Home extends React.Component {
                                 Welcome to Github Issue Browser!
                             </Col>
                             <Col lg={{ span: 8, offset: 2 }} id="description">
-                                Start by simply clicking 'default' or input a custom url.
+                                Start by simply clicking 'default' or input a custom Github url.
                             </Col>
                         </Row>
                     </Container>
                 </section>
 
                 <section className="section-input">
-                    <TextField id="standard-basic" label="Enter github URL" className={classes.textfield} 
-                        color="primary"
-                    />
+                    <Form>    
+                        <Form.Group controlId="exampleForm.ControlInput1" >
+                            <Form.Control type="email" placeholder="Github Issues URL" id="form-github" 
+                                onChange={(e) => this.setState({ GithubURL: e.target.value })}/>
+                        </Form.Group>
+
+                        <span>
+                            <Button variant="primary" id="submit-button" 
+                                onClick={() => console.log('clicked')}>Submit</Button>
+
+                            <Button variant="info" id="default-button" 
+                                onClick={() => console.log('clicked')}>Default</Button>
+                        </span>
+                    </Form>   
                 </section>
             </div>
         )
     }
 }
 
-export default withStyles(styles)(Home);
+export default Home;
